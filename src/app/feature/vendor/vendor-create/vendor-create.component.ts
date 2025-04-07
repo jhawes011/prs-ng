68d87830146d4119294b6bdef593bf4e9f453141
Joe Hawes <jhawes011@gmail.com>
2025-04-07 14:39:41 -0400

    vendor component create

--- src/app/feature/vendor/vendor-create/vendor-create.component.ts
+++ src/app/feature/vendor/vendor-create/vendor-create.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Vendor } from '../../../model/vendor';
+import { VendorService } from '../../../service/vendor.service';
 
 @Component({
   selector: 'app-vendor-create',
   standalone: false,
   templateUrl: './vendor-create.component.html',
-  styleUrl: './vendor-create.component.css'
+  styleUrl: './vendor-create.component.css',
 })
-export class VendorCreateComponent {
+export class VendorCreateComponent implements OnInit, OnDestroy {
+  title: string = 'Vendor Create';
+  newVendor: Vendor = new Vendor();
+  subscription!: Subscription;
+  constructor(private vendorSvc: VendorService, private router: Router) {}
 
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  addVendor(): void {
+    this.subscription = this.vendorSvc.add(this.newVendor).subscribe((resp) => {
+      this.router.navigateByUrl('/vendor-list');
+    });
+  }
 }
